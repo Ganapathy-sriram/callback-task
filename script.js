35ac4472c4a0131df0e7f6a3e6c798c90fe01601
Ganapathy-sriram <110919710+Ganapathy-sriram@users.noreply.github.com>
2022-09-12 11:25:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const countdown = document.querySelector(".countdown");
+let num = 10;
+
+setTimeout(( ) => {
+    countdown.innerText = --num;
+    setTimeout(( ) => {
+        countdown.innerText = --num;
+        setTimeout(( ) => {
+            countdown.innerText = --num;
+            setTimeout(( ) => {
+                countdown.innerText = --num;
+                setTimeout(( ) => {
+                    countdown.innerText = --num;
+                    setTimeout(( ) => {
+                        countdown.innerText = --num;
+                        setTimeout(( ) => {
+                            countdown.innerText = --num;
+                            setTimeout(( ) => {
+                                countdown.innerText = --num;
+                                setTimeout(( ) => {
+                                    countdown.innerText = --num;
+                                    setTimeout(( ) => {
+                                        countdown.innerText = "Happy Independence Day"
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
